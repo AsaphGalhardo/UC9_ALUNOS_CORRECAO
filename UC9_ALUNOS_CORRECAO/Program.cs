4e1700d7f790ceb813faea0e6dabe90334bbb04e
AsaphGalhardo <asaphgalhardo@gmail.com>
2023-03-06 20:24:04 -0300

    criaçao dor

--- UC9_ALUNOS_CORRECAO/Program.cs
+++ UC9_ALUNOS_CORRECAO/Program.cs
@@ -1,2 +1,25 @@
 ﻿string[,] alunos = new string[5, 7];
 int l, c;
+l = 0;
+c = 0;  
+
+for (l =0; l<= 4; l++)
+{
+    Console.WriteLine("Nome do aluno:");
+    alunos[l, c++] = Console.ReadLine();
+    Console.WriteLine("aulas dadas:");
+    alunos[l, c++] = Console.ReadLine();
+    Console.WriteLine("faltas do aluno");
+    alunos[l, c++] = Console.ReadLine();
+    Console.WriteLine("Nome da prova 1 :");
+    alunos[l, c++] = Console.ReadLine();
+    Console.WriteLine("Nome da prova 2 :");
+    alunos[l, c++] = Console.ReadLine();
+    Console.WriteLine("Nome da prova 3 :");
+    alunos[l, c++] = Console.ReadLine();
+    Console.WriteLine("Nota do trabalho :");
+    alunos[l, c++] = Console.ReadLine();
+
+}
+
+
